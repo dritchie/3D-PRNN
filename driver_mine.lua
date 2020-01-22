24f576428a4a670b2b1614ae7e5f931dbb032055
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2020-01-21 21:38:24 -0500

    whoops, should've added this a while ago

---
+++ driver_mine.lua
@@ -0,0 +1,24 @@
+-- revised based on https://github.com/jarmstrong2/handwritingnet/blob/master/driver.lua
+
+require 'cutorch'
+
+local cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Script for training sequence model.')
+
+cmd:option('-inputSize' , 3, 'number of input dimension')
+cmd:option('-hiddenSize' , 400, 'number of hidden units in lstms')
+cmd:option('-lr' , 1e-4, 'learning rate')
+-- cmd:option('-maxlen' , 300, 'max sequence length')
+cmd:option('-maxlen' , 90, 'max sequence length')
+-- cmd:option('-batchSize' , 50, 'mini batch size')
+cmd:option('-batchSize' , 5, 'mini batch size')
+cmd:option('-numPasses' , 1, 'number of passes')
+cmd:option('-valData' , './mydata/val_all.t7', 'filepath for validation data')
+cmd:option('-trainData' , './mydata/train_all.t7', 'filepath for training data')
+
+cmd:text()
+opt = cmd:parse(arg)
+
+dofile('model_mine.lua')
+dofile('train_3dp.lua')
